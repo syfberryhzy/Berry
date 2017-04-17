ba6f3dca47cca2002cbcdcf9d62501785b8725af
syfberryhzy <1913549290@mail.com>
2017-04-17 06:42:54 +0000

    Finish static pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+get('/','StaticPagesController@home');
+get('/help','StaticPagesController@help');
+get('/about','StaticPagesController@about');
